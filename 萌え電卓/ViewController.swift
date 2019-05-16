be95d817d2527aace90f861416ddc116fedd1a59
奥城健太郎 <ken09460308@gmail.com>
2019-05-16 20:35:31 +0900

    Initial Commit

---
+++ 萌え電卓/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  萌え電卓
+//
+//  Created by 奥城健太郎 on 2019/05/16.
+//  Copyright © 2019 奥城健太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
